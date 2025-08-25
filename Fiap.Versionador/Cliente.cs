0db4cd850c39c2f01fce754a8b60b2749099c853
GabrielRossi01 <rossi17006@gmail.com>
2025-08-25 09:32:03 -0300

    Alteração de classe

---
+++ Fiap.Versionador/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Versionador
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public string nome { get; set; }
+        public int nrCEP { get; set; }
+        public string dsEndereco { get; }
+        public string CPF { get; set; }
+
+    }
+}
